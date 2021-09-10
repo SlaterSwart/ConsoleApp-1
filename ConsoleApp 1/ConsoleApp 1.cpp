1a61129b58110effdad78f39f9a443487bf2c8cc
Slater Swart <swartsl@rsd407.org>
2021-09-10 09:33:27 -0700

    Update

--- ConsoleApp 1/ConsoleApp 1.cpp
+++ ConsoleApp 1/ConsoleApp 1.cpp
@@ -1,4 +1,13 @@
-//Says "Hello world"
+/*
+Porject name: Hello World
+Author Name: Slater Swart
+File name: ConsoleApp1.cpp
+porpose: To say Hello world and 3 other lines of code
+Psudocode: Says hello world to you and has a small conversation with you.
+Mantainence log: 
+9/9/21: I wrote all 4 line of code and was able to run it with out issues
+9/10/21: polished the code and added a mantainence log aswell as project name and the rest of the comments.
+*/
 #include <stdio.h>
 #include <conio.h>
 #include <iostream>
