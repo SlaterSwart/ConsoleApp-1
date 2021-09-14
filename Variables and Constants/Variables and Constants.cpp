f51d5811eea68cdc2c5efce46f8b1b183d5acf4e
Slater Swart <swartsl@rsd407.org>
2021-09-14 10:42:21 -0700

    awddfs

---
+++ Variables and Constants/Variables and Constants.cpp
@@ -0,0 +1,16 @@
+// Variables and Constants.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <string>
+#include <stdio.h>
+#include <conio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    printf("Hello world!\n");
+    Int a = 0;
+    const float g = 6.62607004
+
+}
+
