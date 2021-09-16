27040084bd89d32eaa1436c5997f5f3c469dc321
Slater Swart <swartsl@rsd407.org>
2021-09-16 08:55:54 -0700

    I did stuff :D Hi walt

--- Variables and Constants/Variables and Constants.cpp
+++ Variables and Constants/Variables and Constants.cpp
@@ -8,10 +8,22 @@ using namespace std;
 
 int main()
 {
-    printf("Hello world!\n");
-    const float c = 3.0e8;
-    const float g = 9.8:
-    int 
+	printf("hello walt :DDDD\n");
+	int FavoriteNumber = 3;
+	const float g = 3.0e8;
+	const float c = 9.3;
+	char bruh = '30.6';
+	string favoriteColor = "My favorite color is blue";
+	const bool YesYes = 1;
+	string PlacesIveLived = "Washington... that's it";
+		cout << "I have lived in" << PlacesIveLived << endl;
+		cout << "this is that" << g << endl;
+	string favoriteClass = " My favorite class is Programming with Miyoshi!";
+	cout << "what's My favorite class?" << favoriteClass << endl;
+	printf("Am I Huge brain? %i\n", YesYes);
+
+
+
 
 }
 
