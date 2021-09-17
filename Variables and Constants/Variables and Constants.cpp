d89a59790d3af7d4f67ec861f75b6169af5c1b92
Slater Swart <swartsl@rsd407.org>
2021-09-17 09:06:57 -0700

    awdasd

--- Variables and Constants/Variables and Constants.cpp
+++ Variables and Constants/Variables and Constants.cpp
@@ -16,11 +16,29 @@ int main()
 	string favoriteColor = "My favorite color is blue";
 	const bool YesYes = 1;
 	string PlacesIveLived = "Washington... that's it";
-		cout << "I have lived in" << PlacesIveLived << endl;
-		cout << "this is that" << g << endl;
+		cout << "I have lived in " << PlacesIveLived << endl;
+		cout << "this is that " << g << endl;
 	string favoriteClass = " My favorite class is Programming with Miyoshi!";
 	cout << "what's My favorite class?" << favoriteClass << endl;
-	printf("Am I Huge brain? %i\n", YesYes);
+	printf("Am I Huge brain? if yes then 1 if no the 0: %i\n", YesYes);
+	_getch;
+	int HowManyPeopleInMyFam = 6;
+	int HowMuchToastDoIHave = 0;
+	char bruhhh = 'X';
+	char bruuhhh = 'y';
+	double _1 = -12937;
+	double _2 = -8;
+	double _3 = -8712;
+	cout << "this is my first double: " << _1 << endl;
+	cout << "this is my second double:" << _2 << endl;
+	cout << "this is my third double:" << _3 << endl;
+	cout << "numbers, Am i right?" << bruh << bruhhh << bruuhhh << endl;
+
+
+
+
+	return 0;
+
 
 
 
