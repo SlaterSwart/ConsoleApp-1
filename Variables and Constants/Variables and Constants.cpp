a3b02ada091469a408f5851b0cdd0aa3ecaae794
Slater Swart <swartsl@rsd407.org>
2021-09-20 09:38:18 -0700

    I did stuff

--- Variables and Constants/Variables and Constants.cpp
+++ Variables and Constants/Variables and Constants.cpp
@@ -1,5 +1,11 @@
-// Variables and Constants.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+ 
+ 
+ 
+ 
+ 
+
+*/
 #include <string>
 #include <stdio.h>
 #include <conio.h>
@@ -11,36 +17,34 @@ int main()
 	printf("hello walt :DDDD\n");
 	int FavoriteNumber = 3;
 	const float g = 3.0e8;
-	const float c = 9.3;
-	char bruh = '30.6';
+	const float c = 9.8;
+	char bruh = 'x';
 	string favoriteColor = "My favorite color is blue";
-	const bool YesYes = 1;
+	const bool YesYes = true;
 	string PlacesIveLived = "Washington... that's it";
 		cout << "I have lived in " << PlacesIveLived << endl;
 		cout << "this is that " << g << endl;
 	string favoriteClass = " My favorite class is Programming with Miyoshi!";
 	cout << "what's My favorite class?" << favoriteClass << endl;
-	printf("Am I Huge brain? if yes then 1 if no the 0: %i\n", YesYes);
-	_getch;
+	cout << std::boolalpha;
+	cout << "Am I Huge brain? " << YesYes << endl;
+	_getch();
 	int HowManyPeopleInMyFam = 6;
 	int HowMuchToastDoIHave = 0;
-	char bruhhh = 'X';
-	char bruuhhh = 'y';
+	char bruhhh = 'y';
+	char bruuhhh = 'z';
 	double _1 = -12937;
 	double _2 = -8;
 	double _3 = -8712;
 	cout << "this is my first double: " << _1 << endl;
 	cout << "this is my second double:" << _2 << endl;
 	cout << "this is my third double:" << _3 << endl;
-	cout << "numbers, Am i right?" << bruh << bruhhh << bruuhhh << endl;
-
-
-
-
+	cout << "numbers, Am i right?   " << bruh << endl;
+	cout << bruhhh << endl;
+	cout << bruuhhh << endl;
+	_getch();
+	system("cls");
 	return 0;
-
-
-
 
 
 }
