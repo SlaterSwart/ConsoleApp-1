525989ecaa52106d6f602b04fc6e3257d2a3dbc6
Slater Swart <swartsl@rsd407.org>
2021-09-30 09:29:46 -0700

    wad

---
+++ Bus Charter/Bus Charter.cpp
@@ -0,0 +1,26 @@
+// Bus Charter.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+	string input;
+	int peopleOnTheBus;
+
+	printf("How many people do you have on the whole tour?\n");
+	cin >> peopleOnTheBus;
+	_getch();
+	system("CLS");
+
+	printf("is this how many people you have on the tour?: %d\n", peopleOnTheBus);
+
+
+
+
+
+}
--- Random numbers n'stuff/Random numbers n'stuff.cpp
+++ Random numbers n'stuff/Random numbers n'stuff.cpp
@@ -4,24 +4,36 @@
 #include <iostream>
 #include <conio.h>
 #include <string>
+#include <stdio.h>
+#include <stdlib.h>
 using namespace std;
-
 
 int main()
 {
-	string input;
-	int i = 0;
+	srand(0);
+	srand((unsigned)time(NULL));
 
-	while (i < 10)
-	{
-		if (input == "t")
-		{
-			printf("bruh you hit T XD");
-		}
-		else if (input == "hi")
-		{
-			continue;
-		}
-	}
+	printf("Heyy, Do you wanna sea somthnin g cool?\n\t Press space to continue\n");
+	_getch();
+	system("CLS");
+
+	float floatr;
+	float floatrMin = 0.0;
+	float floatrMax = 2.0;
+
+	int pInt;
+	int pIntMin;
+	int pIntMax;
+
+	float pFloat;
+	float pFloatMin;
+	float pFloatMax;
+
+	char pCharacter;
+	char pCharacterMin;
+	char pCharacterMax;
+
+	floatr = floatrMin + (float)(rand()) / 
 	
+
 }
