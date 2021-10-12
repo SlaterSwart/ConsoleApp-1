294350fc587faa9ceadadb53c70c587846e428d9
Slater Swart <swartsl@rsd407.org>
2021-10-12 10:47:16 -0700

    start

---
+++ Maze/Maze.cpp
@@ -0,0 +1,10 @@
+// Maze.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+	printf("this is a maze, and joey is SUPER HOT!!!!!\n");
+
+}
