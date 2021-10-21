0f537c6f89163fcf09a2d68c214b6f846506804b
Slater Swart <swartsl@rsd407.org>
2021-10-21 08:54:42 -0700

    okin blykyers

--- Roman numbers 2/Roman numbers 2.cpp
+++ Roman numbers 2/Roman numbers 2.cpp
@@ -5,24 +5,60 @@
 #include <time.h>
 #include <conio.h>
 #include <string>
+#include <sstream>
 using namespace std;
 
 int main()
 {
-	string input;
+	int input;
 	string yesno;
 	int thousands;
 	int hundreds;
 	int tens;
 	int ones;
+	int hund;
 
 	printf("Enter the number you want to process to roman numerals\n");
 	cin >> input;
 	printf("%s\n");
 	_getch();
 	system("CLS");
-	
-	(input/100)
 
+	thousands = input / hund;
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	printf("Olin is really cool.\n); 
+	return 0;
 }
 
