98faf117ec3b4edc6c4bc2f0a3267d911841f4ec
Slater Swart <swartsl@rsd407.org>
2021-10-28 19:39:13 -0700

    asd

---
+++ I'm counting on you string/I'm counting on you string.cpp
@@ -0,0 +1,22 @@
+// I'm counting on you string.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+	string name;
+	string userText;
+
+	printf("Type the sentence that you would like analyzed\n");
+	cin > userText;
+
+
+
+
+}
+
