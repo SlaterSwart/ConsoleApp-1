1bfa650ccef17838e4d3bf67a305d2f09cea7c03
Slater Swart <swartsl@rsd407.org>
2021-11-02 10:48:59 -0700

    awd

--- ASCII Art animate/ASCII Art animate.cpp
+++ ASCII Art animate/ASCII Art animate.cpp
@@ -11,7 +11,16 @@ using namespace std;
 
 int main()
 {
-
-
+printf("           (                 ,&&&.	endl");
+printf("            )                .,.&&	endl");
+printf("           (  (              \\=__/	endl");
+printf("               )             ,'-'.	endl");
+printf("         (    (  ,,      _.__|/ /|	endl");
+printf("          ) /\\ -((------((_|___/ |	endl");
+printf("        (  // | (`'      ((  `'--|	endl");
+printf("      _ -.;_/ \\\\--._      \\\\ \\-._/.	endl");
+printf("     (_;-// | \\ \\-'.\\    <_,\\_\\`--'|endl");
+printf("     ( `.__ _  ___,')      <_,-'__,'endl");
+printf("jrei  `'(_ )_)(_)_)')				endl");
 }
 
