509bc0fd597d9b59d2429a3bc49fbfdd08336bca
Slater Swart <swartsl@rsd407.org>
2021-12-10 09:06:34 -0800

    aedxzfsdf

--- Looping maze/Looping maze.cpp
+++ Looping maze/Looping maze.cpp
@@ -22,6 +22,7 @@ int main()
 	bool room7;
 	bool room8;
 	bool room9;
+	bool quit = false;
 	printf("what is your name?\n");
 	cin >> name;
 	printf("Hi %s!\n", name.c_str());
@@ -33,72 +34,124 @@ int main()
 	getch();
 	system("CLS");
 	room1 = true;
-	while (room1 == true) { // Start
-		printf(" you are in room 1.you can go right or down 1 for right 2 for down\n");
-		cin >> input;
-		if (input == 1) {
-			room1 = false;
-			room2 = true;
-			break;
+	while (quit == false) {
+		while (room1 == true) { // Start
+			printf(" you are in room 1.you can go right or down 1 for right 2 for down\n");
+			cin >> input;
+			if (input == 1) {
+				room1 = false;
+				room2 = true;
+				break;
+			}
+			if (input == 2) {
+				room1 = false;
+				room4 = true;
+				break;
+			}
+			system("CLS");
 		}
-		if (input == 2) {
-			room1 = false;
-			room4 = true;
-			break;
+
+		while (room2 == true) {
+			printf(" you are in room 2. you can go left, down, and right. 1 for left. 2 for down. 3 for right. \n");
+			cin >> input;
+			if (input == 1) {
+				room1 = true;
+				room2 = false;
+				break;
+			}
+			else if (input == 2) {
+				room5 = true;
+				room2 = false;
+				break;
+			}
+			else if (input == 3) {
+				room3 = true;
+				room2 = false;
+				break;
+			}
+			system("CLS");
 		}
-	}
 
-	while (room2 == true) {
-		printf(" you are in room 2. you can go left, down, and right. 1 for left. 2 for down. 3 for right. \n");
-		cin >> input;
-		if (input == 1) {
-			room1 = true;
-			room2 = false;
-			break;
+		while (room3 == true) {
+			printf(" you are in room 3. you can go left. press 1 to go left\n");
+			cin >> input;
+			if (input == 1) {
+				room2 = true;
+				room3 = false;
+				break;
+			}
+			system("CLS");
 		}
-		else if (input == 2) {
-			room5 = true;
-			room2 = false;
-			break;
+
+		while (room4 == true) {
+			printf(" you are in room 4. you can go up, right and down. 1 for up. 2 for right. 3 for down.\n");
+			cin >> input;
+			if (input == 1) {
+				room1 = true;
+				room4 = false;
+				break;
+			}
+			else if (input == 2) {
+				room5 = true;
+				room4 = false;
+				break;
+			}
+			else if (input == 3) {
+				room7 = true;
+				room4 = false;
+				break;
+			}
+			system("CLS");
 		}
-		else if (input == 3) {
-			room3 = true;
-			room2 = false;
-			break;
+
+		while (room5 == true) {
+			printf(" you are in room 5. you can go left, up and right. 1 for left. 2 for up 3 for right.\n");
+			cin >> input;
+			if (input == 1) {
+				room4 = true;
+				room5 = false;
+				break;
+			}
+			else if (input == 2) {
+				room2 = true;
+				room5 = false;
+				break;
+			}
+			system("CLS");
 		}
-	}
 
-	while (room3 == true) {
-		printf(" you are in room 3. you can go left. press 1 to go left\n");
-		cin >> input;
-		if (input == 1) {
-			room2 = true;
-			room3 = false;
+		while (room6 == true) {
+			printf(" you are in room 6. you can go go left. 1 for left.\n");
+			cin >> input;
+			if (input == 1) {
+				room6 = false;
+				room5 = true;
+				break;
+			}
 		}
-	}
 
-	while (room4 == true) {
-		printf(" you are in room 4. you can go up, left and down.\n");
-	}
+		while (room7 == true) {
+			printf(" you are in room 7. you can go up and right. 1 for up. 2 for right \n");
+			cin >> input;
+			if (input == 1) {
+				room4 = true;
+				room7 = false;
+				break;
+			}
+			else if (input == 2) {
+				room8 = true;
+				room7 = false;
+				break;
+			}
+		}
 
-	while (room5 == true) {
-		printf(" you are in room 5. you can go left, up and right.");
-	}
+		while (room8 == true) {
+			printf(" you are in room 8. you can go left and right.\n");
+		}
 
-	while (room6 == true) {
-		printf(" you are in room 6. you can go go left.\n");
+		while (room9 == true) {
+			printf(" you are in room 9. you have beat the game! would you like to got back? 1 for yes 2 for \n");
+		}
+		return 0;
 	}
-
-	while (room7 == true) {
-		printf(" you are in room 7. you can go up and left.\n");
-	}
-
-	while (room8 == true) {
-		printf(" you are in room 8. you can go left and right.\n");
-	}
-
-	while (room9 == true) {
-		printf(" you are in room 9. you have beat the game! would you like to got back? 1 for yes 2 for \n");
-	}
-	return 0;
 }
