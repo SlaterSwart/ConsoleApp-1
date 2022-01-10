abad1492265a470798ce50d0ffca52fef00345b1
Slater Swart <swartsl@rsd407.org>
2022-01-10 09:38:42 -0800

    awddfsf

--- Fortune cookie/Fortune cookie.cpp
+++ Fortune cookie/Fortune cookie.cpp
@@ -7,16 +7,32 @@
 #include <string>
 #include <sstream>
 #include <Windows.h>
+
 using namespace std;
 
-void FortuneCookie(int num, string & fortune);
-string FortuneCookie(int num);
-int main()
-{
-   
-
-
-
-
-
+string fortunethingcookie(int choice) {
+	string fortunes;
+	switch (choice) {
+		case 1:
+			You will find a bushel of money
+		case 2:
+			Your smile will tell you what makes you feel good.
+		case 3:
+			The best year - round temperature is a warm heart and a cool head
+		case 4:
+			It could be better, but it’s good enough.
+		case 5:
+			You will find a thing.It may be important
+		case 6:
+			Two days from now, tomorrow will be yesterday.
+		case 7:
+			Stop eating now. Food poisoning no fun.
+		case 8:
+			Person who eat fortune cookie get lousy dessert.
+		case 9:
+			You will soon have an out of money experience.
+		case 10:
+			Person who rests on laurels gets thorn in backside.
+		 
+	}
 }
--- root Reducer/root Reducer.cpp
+++ root Reducer/root Reducer.cpp
@@ -24,8 +24,10 @@ int main()
 	int factor;
 	printf("Enter the number that you want to get the root of.\n");
 	cin >> num1;
-	sqrt(factor);
-	cout << "here is your root :)" << factor;
+	factor = rootReducer(num1);
+	num1 = num1 / factor;
+	num1 = num1 / factor;
+	cout << "here is your root(s) :)" << factor << " " << num1 << endl;
 	return 0;
 
 }
