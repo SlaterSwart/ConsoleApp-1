7543c2f7b14c54dc8a8a2cd4df0a9e380296f75c
Slater Swart <swartsl@rsd407.org>
2022-02-26 21:17:29 -0800

    awd

--- search a Vector/search a Vector.cpp
+++ search a Vector/search a Vector.cpp
@@ -2,9 +2,40 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+//if ur looking at this you should make all your programs time complex smh imao
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+struct info {
+    string name;
+    int age;
+    int height;
+    int weight;
+    string phoneNumber;
+};
+
+int main() {
+    
+    vector <info> ppls;
+
+    ifstream in;
+    in.open("Data.txt");
+
+    string temp;
+
+    if (!in.is_open()) {
+        cout << "File not found\n";
+    }
+
+    info person;//temporary instance of the struct
+
+    while (getline(in, temp, '|')) {
+
+    }
+    for (int i = 0; i < ppls.size(); i++) {
+        cout << ppls[i].name << " " << ppls[i].age << " " << ppls[i].height << " " << ppls[i].weight << " " << ppls[i].phoneNumber << endl;
+    }
+    return 0;
 }
-
